d0f984311a21f7c5254ffe6d513598ce1cd3e03a
Liam Bussey <reboot20o@users.noreply.github.com>
2023-06-16 11:14:56 -0230

    Fix

--- src/modules/db_remove.py
+++ src/modules/db_remove.py
@@ -36,12 +36,12 @@ def get_set(text):
     for card in df['card_sets']:
         try:
             # codes.append(next((item['set_code'] for item in card if item["set_name"].endswith(pack)), "Err"))
-            codes.append([item['set_code'] for item in card if item['set_name'].endswith(pack)])
+            codes.extend((item['set_code'] for item in card if item['set_name'].endswith(pack)))
         except TypeError:
             codes.append("Err")
         try:
             # rarity.append(next((item['set_rarity_code'] for item in card if item["set_name"].endswith(pack)), "Err"))
-            rarity.append([item['set_rarity_code'] for item in card if item['set_name'].endswith(pack)])
+            rarity.extend((item['set_rarity_code'] for item in card if item['set_name'].endswith(pack)))
         except TypeError:
             rarity.append("Err")
 
@@ -52,9 +52,13 @@ def get_set(text):
         col = cols.get_loc(card)
         df.at[col, 'id'] = str(card).zfill(8)
 
+    cols = df.columns.tolist()
+    cols.remove("name")
+    cols.remove("id")
+
+    df = df.drop(columns=cols)
     df = pd.concat([df, setCodes, setRarity], axis=1)
-    df = df[["id", "name", "set_code", "rarity"]]
-    df = df.explode(['set_code', 'rarity'])
+    df = df[["id", "set_code", "name", "rarity"]]
     df = df.sort_values(by='set_code').reset_index(drop=True)
 
     if df[df.duplicated(subset=['set_code'])].size > 0:
@@ -82,8 +86,7 @@ def set_info(sets):
         items, codes = [], []
         for dicts in sets:
             set_code = dicts['set_code'].split('-')
-            rarity = dicts['set_rarity_code']
-            if (c[set_code[0]] == 1 or 'EN' in set_code[1] or len(set_code[1]) == 3) & (set_code[0] not in codes or ):
+            if c[set_code[0]] == 1 or 'EN' in set_code[1] or len(set_code[1]) == 3:
                 items.append(dicts)
                 codes.append(set_code[0])
         return items
@@ -134,44 +137,68 @@ def get_info(df):
     return all_cards, card_sets, banlist, card_formats
 
 def get_info_2(df):
-    df = df.convert_dtypes(convert_integer=True) # Convert columns with numeric data to integers
-    dates = pd.json_normalize(df['misc_info'].apply(lambda x: x[0])) # Extract dates
-    df = pd.concat([df, dates.drop(list(set(dates.columns)-{'formats', 'tcg_date', 'ocg_date'}), axis=1)], axis=1) # Add dates and format to dataframe
-    df = df.reindex(columns=['id', 'name', 'type', 'desc', 'race', 'archetype', 'atk', 'def', 'level', 'attribute', 'scale', 'linkval',
-                             'tcg_date', 'ocg_date', 'formats', 'banlist_info', 'card_sets']) # Re-index dataframe
-    cards = df.replace({pd.NA: None}).to_numpy() # Replaces NaN with None type for SQLite and convert dataframe to numpy array
+    df = df.replace({np.nan: None})  # Replaces NaN with None type so SQLite thinks it's Null
+    cards = df.to_numpy()
+    ints = ['atk', 'def', 'level', 'scale', 'linkval']  # Columns with numeric data
     cols = list(df.columns)
-
-    meta = {'all_cards': [], 'card_sets': [], 'card_formats': [], 'banlist': []} # Dictionary to contain card metadata
+    remove = ['card_sets', 'card_images', 'card_prices', 'misc_info', 'banlist_info', 'linkmarkers',
+              'frameType']  # Columns to remove
+    all_cards = []  # List of all cards represented as tuples
+    card_sets = []  # Set metadata
+    card_formats = []
+    banlist = []
 
     for i in range(df.shape[0]):
         ban = {'ban_tcg': None, 'ban_ocg': None, 'ban_goat': None}
         card = {key: val for key, val in zip(cols, cards[i])}  # Initialize dictionary with data for single card
-        sets = card['card_sets']
-        formats = card['formats']
+        card['tcg'], card['ocg'] = None, None
+
+        for j in ints:
+            try:
+                if card[j] is not None:
+                    card[j] = int(card[j])  # Change numeric data from String to Int
+            except KeyError:
+                card[j] = None
+        
         card['id'] = str(card['id']).zfill(8)  # Ensure the card password is 8 characters long by front filling with 0's
+        sets = card['card_sets']
+        formats = card['misc_info'][0]['formats']
 
-        if card['banlist_info'] is not None:  # Check if card is on any banlists
-            ban.update(card['banlist_info'])
+        if 'tcg_date' in card['misc_info'][0].keys():  # Check if card was released in the TCG or OCG
+            card['tcg'] = card['misc_info'][0]['tcg_date']
+        if 'ocg_date' in card['misc_info'][0].keys():
+            card['ocg'] = card['misc_info'][0]['ocg_date']
+        try:
+            if card['banlist_info'] is not None:  # Check if card is on any banlists
+                ban.update(card['banlist_info'])
+        except:
+            pass
+        
+        try:
+            for x in remove:  # Remove irrelevant columns
+                card.pop(x)
+        except:
+            pass
 
-        meta['all_cards'].append(tuple(card.values())[:-3])  # Add card data to list of all cards, removing formats, banlist, and card_sets columns
-        meta['banlist'].append((card['id'], card['name']) + tuple(ban.values()))
+        all_cards.append(tuple(card.values()))  # Add card data to list of all cards
+        banlist.append((card['id'], card['name']) + tuple(ban.values()))
 
         if sets is not None:
             items = set_info(sets)
-            vals = [(elem['set_code'].split('-')[0], elem['set_code'], elem['set_rarity'], elem['set_rarity_code']) for elem in items]  # Proper formatting for set info
+            vals = [(elem['set_code'].split('-')[0], elem['set_code'], elem['set_rarity'], elem['set_rarity_code']) for
+                    elem in items]  # Proper formatting for set info
             for row in vals:
-                meta['card_sets'].append((card['id'], card['name']) + row)
+                card_sets.append((card['id'], card['name']) + row)
         
         for elem in formats:
-            meta['card_formats'].append((card['id'], card['name'], elem))
+            card_formats.append((card['id'], card['name'], elem))
 
-    return meta
+    return all_cards, card_sets, banlist, card_formats
 
 
 def insert_info(con, df):
     """Method re-populates card database. Must first format data to consistent form."""
-    all_cards, card_sets, card_formats, banlist = get_info(df).values()
+    all_cards, card_sets, banlist, card_formats = get_info(df)
     # Insert data into database
     insert_rows(con, 'insert into all_cards values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)', all_cards)
     insert_rows(con, 'insert into sets values (?,?,?,?,?,?)', card_sets)
